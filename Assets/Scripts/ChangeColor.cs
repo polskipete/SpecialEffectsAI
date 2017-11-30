884040c48c648d4f579c59e633ba5c48ade376e9
polskipete <pete885@gmail.com>
2017-11-29 20:34:23 -0500

    Basic/UI
    Transfer colors between psystems, GUI(Sliders/Buttons), code to change colors

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    public GameObject head;
+    private ColorGen otherScripts;
+
+    private float hSliderValueR;
+    private float hSliderValueG;
+    private float hSliderValueB;
+    private float hSliderValueA;
+
+    
+
+
+    public ParticleSystem pEffect;
+    // Use this for initialization
+    void Start () {
+
+        pEffect= GetComponent<ParticleSystem>();
+
+        otherScripts = head.GetComponent<ColorGen>();
+        
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        var main = pEffect.main;
+        hSliderValueR = otherScripts.hSliderValueR;
+        hSliderValueG = otherScripts.hSliderValueG;
+        hSliderValueB = otherScripts.hSliderValueB;
+        hSliderValueA = otherScripts.hSliderValueA;
+        //Debug.Log(hSliderValueG);
+        main.startColor = new Color(hSliderValueR, hSliderValueG, hSliderValueB, hSliderValueA+.5f);
+    }
+    void OnGUI() {
+        
+    }
+
+}
---
+++ Assets/Scripts/ColorGen.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGen : MonoBehaviour {
+    public  ParticleSystem head;
+    public float hSliderValueR ;
+    public float hSliderValueG ;
+    public float hSliderValueB ;
+    public float hSliderValueA ;
+
+    public float hSliderValueRMax;
+    public float hSliderValueGMax;
+    public float hSliderValueBMax;
+    public float hSliderValueAMax;
+
+    public float hSliderValueRMin;
+    public float hSliderValueGMin;
+    public float hSliderValueBMin;
+    public float hSliderValueAMin;
+
+    void Start()
+    {
+        head = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        
+        var main = head.main;
+        main.startColor = new Color(hSliderValueR, hSliderValueG, hSliderValueB, hSliderValueA);
+            }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(25, 40, 100, 30), "Red");
+        GUI.Label(new Rect(25, 70, 100, 30), "Green");
+        GUI.Label(new Rect(25, 100, 100, 30), "Blue");
+        GUI.Label(new Rect(25, 130, 100, 30), "Alpha");
+
+        hSliderValueR = GUI.HorizontalSlider(new Rect(95, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
+        hSliderValueG = GUI.HorizontalSlider(new Rect(95, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
+        hSliderValueB = GUI.HorizontalSlider(new Rect(95, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
+        hSliderValueA = GUI.HorizontalSlider(new Rect(95, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
+
+
+        if (GUI.Button(new Rect(20, 205, 50, 30), "Min"))
+            {
+                hSliderValueRMin= GUI.HorizontalSlider(new Rect(95, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
+                hSliderValueGMin = GUI.HorizontalSlider(new Rect(95, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
+                hSliderValueBMin = GUI.HorizontalSlider(new Rect(95, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
+                hSliderValueAMin = GUI.HorizontalSlider(new Rect(95, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
+                Debug.Log(hSliderValueRMin);
+                Debug.Log(hSliderValueBMin);
+            }
+        if (GUI.Button(new Rect(120, 205, 50, 30), "Max"))
+            {
+                hSliderValueRMin = GUI.HorizontalSlider(new Rect(95, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
+                hSliderValueGMin = GUI.HorizontalSlider(new Rect(95, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
+                hSliderValueBMin = GUI.HorizontalSlider(new Rect(95, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
+                hSliderValueAMin = GUI.HorizontalSlider(new Rect(95, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
+            }
+        if (GUI.Button(new Rect(60, 255, 65, 30), "Generate"))
+            {
+            Debug.Log("AI NOT DONE");
+
+        }
+
+    }
+}
+    
+
